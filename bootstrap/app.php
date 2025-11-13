10604145e189ba6be54981ba209e64a0416b62f9
HaShiRaMaReiZo <zwemannhtet65@gmail.com>
2025-11-13 16:43:16 +0630

    ...

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Auth\AuthenticationException;
-use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
